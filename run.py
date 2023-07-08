c7c4e29ecb9673430e08620ff3dc9d87e84cc5eb
kswelder <welderhayne2000@gmail.com>
2023-07-08 14:19:22 -0300

    Try get no sponsored products

--- run.py
+++ run.py
@@ -4,8 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import csv
 
-def scrap_sponsored_products(arg):
-    print("Defnie arguments")
+def scrap(arg):
+    print("Scraping sponsored products\nDefnie arguments")
     options = Options()
     options.add_argument("--headless")
     driver = webdriver.Chrome(options)
@@ -65,17 +65,59 @@ def scrap_sponsored_products(arg):
             pass
         sponsored_products.append(product)
 
-    print(len(sponsored_products))
+    print("==============================================================\n")
+    print("Scraping no sponsored products\nDefnie arguments")
 
-    driver.close()
+    print("Get Grid element")
 
-    return sponsored_products
+    element = driver.find_element(By.XPATH, "/html/body/div[6]/div/div[4]/div[3]/div/div[3]/div[2]/div[2]/div")
+    elements = element.find_elements(By.CLASS_NAME, "sh-dgr__gr-auto sh-dgr__grid-result")
+
+    no_sponsored_products = []
+
+    for inList in elements:
+        product = {
+            'name': '',
+            'category': arg,
+            'promotion': False,
+            'price': '',
+            'linkShop': '',
+            'nameShop': ''
+        }
+        try:
+            inList.find_element(By.CLASS_NAME, "Ib8pOd")
+            product['promotion'] = True
+        except:
+            pass
+        try:
+            product['name'] = inList.find_element(By.TAG_NAME, "h3").text
+        except:
+            pass
+        try:
+            product['linkShop'] = inList.find_element(By.CLASS_NAME, "Lq5OHe eaGTj translate-content").get_attribute("href")
+        except:
+            pass
+        try:
+            product['price'] = inList.find_element(By.CLASS_NAME, "a8Pemb OFFNJ").text
+        except:
+            pass
+        try:
+            product['nameShop'] = inList.find_element(By.CLASS_NAME, "aULzUe IuHnof").text
+        except:
+            pass
+        no_sponsored_products.append(product)
+
+    result_elements = {
+        'sponsored': sponsored_products,
+        'no_sponsored': no_sponsored_products
+    }
+    return result_elements
 
 print("Start program.")
 
-list_sponsored_products = scrap_sponsored_products("computador gamer")
+print("Scraping Data")
+results = scrap('computador gamer')
 
-print("End Scraping\nMake file")
 if path.exists('./products.csv'):
     file = open('products.csv', 'w')
     whiter = csv.writer(file)
@@ -84,14 +126,39 @@ else:
     file = open('products.csv', 'w')
     whiter = csv.writer(file)
 print("Write data in file")
-for element in list_sponsored_products:
-    elements = [
-        element['name'],
-        element['category'],
-        element['price'],
-        element['nameShop'],
-        element['linkShop'],
-        element['promotion'],
-        'SPONSORED'
-    ]
-    whiter.writerow(elements)
+
+list_sponsored_products = results['sponsored']
+
+if len(list_sponsored_products) > 0:
+    print("Write Sponsored products")
+    for element in list_sponsored_products:
+        elements = [
+            element['name'],
+            element['category'],
+            element['price'],
+            element['nameShop'],
+            element['linkShop'],
+            element['promotion'],
+            'SPONSORED'
+        ]
+        whiter.writerow(elements)
+
+list_no_sponsored_products = results['no_sponsored']
+
+if len(list_no_sponsored_products) > 0:
+    print("Write no sponsored products")
+    for element in list_no_sponsored_products:
+        elements = [
+            element['name'],
+            element['category'],
+            element['price'],
+            element['nameShop'],
+            element['linkShop'],
+            element['promotion'],
+            'NO_SPONSORED'
+        ]
+        whiter.writerow(elements)
+
+file.close()
+
+print("End program")
