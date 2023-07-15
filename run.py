850474a21e9cd5aad246a39fbeb9c06dfe5e60d5
kswelder <welderhayne2000@gmail.com>
2023-07-15 20:49:21 -0300

    Refactory list

--- run.py
+++ run.py
@@ -5,29 +5,35 @@ from make_file import make_file
 def scrap():
     print('Wellcome to scraping products to Google shopping')
     product = input('Search product: ')
+    size_list_sponsored = int(input('Size list sponsored products: '))
+    size_list_no_sponsored = int(input('Size list no sponsored products: '))
 
-    products = [
-        scrap_sponsored_products(product),
-        scrap_no_sponsored_products(product)
-    ]
+    sponsored = scrap_sponsored_products({
+        'name': product,
+        'size': size_list_sponsored
+    })
+    no_sponsored = scrap_no_sponsored_products({
+        'name': product,
+        'size': size_list_no_sponsored
+    })
 
     print('Save products in csv file ?\nY, n')
     save = input()
 
     if save in ['Y', 'y']:
-        make_file(products)
+        make_file(sponsored, no_sponsored)
     else:
         print('Print list products ?\nY, n')
         listPrint = input()
         if listPrint in ['Y', 'y']:
-            listPrint = input('Are you sure you want to print ', len(products[0]) + len(products[1]), ' results to the screen?\nY, n')
+            listPrint = input('Are you sure you want to print ', len(sponsored) + len(no_sponsored), ' results to the screen?\nY, n')
             if listPrint in ['Y', 'y']:
                 for element in products:
                     print(element)
             else:
                 save = input('You want save in csv file?\nY, n\n')
                 if save in ['Y', 'y']:
-                    make_file(products)
+                    make_file(sponsored, no_sponsored)
 
 if __name__ == '__main__':
     scrap()
